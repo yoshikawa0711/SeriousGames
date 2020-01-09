021c4ec04a4e1faa40b42d84a79978b7beae6518
yoshikawa0711 <b1017166@fun.ac.jp>
2020-01-09 11:34:04 +0900

    [add]スタート画面の実装

--- MoralHazard/Assets/Scripts/QuizController.cs
+++ MoralHazard/Assets/Scripts/QuizController.cs
@@ -9,18 +9,13 @@ public class QuizController : MonoBehaviour
     public GameObject answer2_object = null;
     public GameObject answer3_object = null;
 
-    private string[] answers = {
-        "1  まだできてないの？",
-        "2  疲れてるの？",
-        "3  晩酌しながら待つ"
-    };
-
     void Start()
     {
+        string[ , ] answers = StoryController.getAnswers();
         /* ボタンに選択肢を反映 */
-        answer1_object.GetComponentInChildren<Text>().text = answers[0];
-        answer2_object.GetComponentInChildren<Text>().text = answers[1];
-        answer3_object.GetComponentInChildren<Text>().text = answers[2];
+        answer1_object.GetComponentInChildren<Text>().text = answers[0, 0];
+        answer2_object.GetComponentInChildren<Text>().text = answers[1, 0];
+        answer3_object.GetComponentInChildren<Text>().text = answers[2, 0];
 
     }
 
